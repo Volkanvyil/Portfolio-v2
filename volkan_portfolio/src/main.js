8a38c4e323528ed5344618a67026264752d5ef05
Volkanvyil <volk0053@easv365.dk>
2021-05-06 03:05:50 +0200

    sd

--- volkan_portfolio/src/main.js
+++ volkan_portfolio/src/main.js
@@ -4,9 +4,11 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 
+
 createApp(App).use(store).use(router).mount('#app')
 
 new Vue({
     vuetify,
     render: h => h(App)
-    }).$mount('#app');
+    }).$mount('#app'); 
+
