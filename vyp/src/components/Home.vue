d6b6b3b1c621b4aa5ec6d35f8c13fa1dbfe36d97
Volkanvyil <volk0053@easv365.dk>
2021-05-06 17:48:05 +0200

    2sd

---
+++ vyp/src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-container>
+
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
