e862ea48a0e65906a2bd80d412c33e6ff9c0029d
Volkanvyil <volk0053@easv365.dk>
2021-05-07 03:49:44 +0200

    asd

--- vyp/public/forms/contact.php
+++ vyp/public/forms/contact.php
@@ -2,7 +2,7 @@
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'volkanvyil.work@outlook.dk';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email.php' )) {
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
